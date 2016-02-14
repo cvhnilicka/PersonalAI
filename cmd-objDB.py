ff9239719830c783741c834e47f91aad6d9d21a5
Cormick Hnilicka <cvhnilicka@wisc.edu>
2016-02-13 21:14:10 -0600

    db start

---
+++ cmd-objDB.py
@@ -0,0 +1,15 @@
+
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+
+
+class Tree(object):
+	print("tree)
