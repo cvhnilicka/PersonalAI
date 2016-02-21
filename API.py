6d9622cae8e924442fdc0e10cc5561d3f75b1689
Cormick Hnilicka <Cormick@wirelessprvnat-172-17-196-243.near.illinois.edu>
2016-02-20 20:06:53 -0600

    weather api call

--- API.py
+++ API.py
@@ -5,9 +5,10 @@ class API:
 	
 	underground_key = "9336d91494799de4c1a02efcf9c84368"
 	google_api_key = "AIzaSyBVU2iI94OgOpwk_GvxEBQroZ-ryM6Znpw"
-
+	
+	
 	validAPIs = {
-	"weather" : None
+	"weather" : True 
 
 }
 	@staticmethod
@@ -23,13 +24,15 @@ class API:
 
 		r = requests.get(api)
         	feed = r.json()
+		print r
+		print feed
 
         	#save the weather summary
         	weather_whole = feed["weather"][0]
         	weather_summary = weather_whole["main"]
         	#current temperature
         	temp = feed["main"]["temp"]
-        	temp = (temp * (9/5) - 459.67)
+        	temp = ((temp - 273.15) * 1.8) + 32.00
 
         	#current description of weatheer
         	description = weather_whole["description"]
@@ -37,5 +40,5 @@ class API:
         	#current wind information
 	        wind_speed = feed["wind"]["speed"]
 
-		print temp
-API.weatherApi(None)
+		print temp, wind_speed, weather_summary, description
+
